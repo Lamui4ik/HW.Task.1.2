60a652e144c4562c8932812d10edbc421db0a0dd
Lamueva <marinalamueva@gmail.com>
2021-03-04 22:24:26 +0800

    Добавлены файлы Main и Worker

--- Worker.java
+++ Worker.java
@@ -1,15 +1,10 @@
 package com.company;
 
 public class Worker {
+
     @FunctionalInterface
     public interface OnTaskDoneListener {
         void onDone(String result);
-    }
-
-    private OnTaskDoneListener callback;
-
-    public Worker(OnTaskDoneListener callback) {
-        this.callback = callback;
     }
 
     @FunctionalInterface
@@ -17,13 +12,15 @@ public class Worker {
         void OnError(String result);
     }
 
+    private OnTaskDoneListener callback;
     private OnTaskErrorListener errorCallback;
 
-    public Worker(OnTaskErrorListener errorCallback) {
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
         this.errorCallback = errorCallback;
     }
 
-    public void start(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+    public void start() {
         for (int i = 0; i < 100; i++) {
             if (i != 33) {
                 callback.onDone("Task " + i + " is done");
